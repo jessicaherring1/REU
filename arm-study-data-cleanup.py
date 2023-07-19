9bd740ff98bee8a9c736ffa01695a1a530a00470
Jessica H <jessica.herring123@gmail.com>
2023-07-19 11:55:46 -0700

    updates

--- arm-study-data-cleanup.py
+++ arm-study-data-cleanup.py
@@ -8,16 +8,17 @@ from openpyxl import Workbook, load_workbook
 oWorkbook = load_workbook(filename="Documents/REU/old-arm-study-data.xlsx")
 oSheet = oWorkbook.active
 
-# updated workbook varibales 
+# updated workbook (formatted how i like) 
 uWorkbook = Workbook()
 uSheet = uWorkbook.active
 
-#simplified workbook variables 
+#simplified workbook (updated from uWorkbook, averages all participant answers per row) 
 sWorkbook = Workbook()
-sSheet = uWorkbook.active
+sSheet = sWorkbook.active
 
-# adds blank row to help with pandas processing
+# adds "blank" row to help with pandas processing
 uSheet["A1"] = "1Title"
+
 
 ##CONVERT ALL COLUMNS INTO ROWS 
 
@@ -67,53 +68,70 @@ uSheet.delete_cols(2, 2)
 # save into new excel file
 uWorkbook.save(filename="Documents/REU/updated-arm-study-data.xlsx")
 
+
 ## SIMPLIFIED WORKBOOK
 
-comp = ["1", "2", "3", "4", "5", "6"]
-disc = ["7", "8", "9", "10", "11", "12"]
-
-
-rows = uSheet.iter_rows(min_row=2, max_row=6, max_col=1, values_only=True)
-values = [row[0] for row in rows]
-print(values)
-avg = sum(values) / len(values)
+num_col = uSheet.max_column
 
 #for each row in the sheet
-#for row in uSheet:
+for i in range (1, (uSheet.max_row + 1)):
+    rows = uSheet.iter_cols(min_row=i, max_col=1, values_only=True)
+    values = [row[0] for row in rows]
 
-    #value in first column
-#    string = str(row[0].value) 
+    print(values[0])
 
-    # first letter of value in first column
-#    first_letter = string[0]
+    location = "A" + str(i)
+    print(location)
+    sSheet[location] = values[0]
 
-    # last letter of value in first column
-#    last_2letter = string[::-1][0:2]
 
-    #removes "11" and "12" questions
- #   if not last_2letter.isnumeric():
- #       last_letter = string[-1]
-        #only gets questions surrounding competency
-#        if "comp" in string and any([x in last_letter for x in comp]):
- #           for i in range(uSheet.max_column):
- #               sSheet.add(row)
+#for each row in the sheet
+for i in range (2, (uSheet.max_row + 1)):
+    rows = uSheet.iter_cols(min_row=i, min_col=2, max_col=num_col, values_only=True)
+    values = [row[0] for row in rows]
+
+    avg = sum(values) / len(values)
+
+    location = "B" + str(i)
+
+    sSheet[location] = avg
+
+sSheet["B1"] = "Average"
+
+
+# save into new excel file
+sWorkbook.save(filename="Documents/REU/simplified-arm-study-data.xlsx")
+
                 
 
 
-## CREATING A CSV FILE
+## CREATING CSV FILES
 #https://www.studytonight.com/post/converting-xlsx-file-to-csv-file-using-python#:~:text=You%20can%20use%20openpyxl%20to,standard%20file%20I%2FO%20operations.
-csv = open("Documents/REU/updated-arm-study-data.csv", "w+")
+uCsv = open("Documents/REU/updated-arm-study-data.csv", "w+")
+sCsv = open("Documents/REU/simplified-arm-study-data.csv", "w+")
 
+#for updated sheet
 for row in uSheet.rows:
     l = list(row)
     for i in range(len(l)):
         if i == len(l) - 1:
-            csv.write(str(l[i].value))
-            csv.write('\n')
+            uCsv.write(str(l[i].value))
+            uCsv.write('\n')
         else:
-            csv.write(str(l[i].value) + ',')
+            uCsv.write(str(l[i].value) + ',')
+
+#for simplified sheet
+for row in sSheet.rows:
+    l = list(row)
+    for i in range(len(l)):
+        if i == len(l) - 1:
+            sCsv.write(str(l[i].value))
+            sCsv.write('\n')
+        else:
+            sCsv.write(str(l[i].value) + ',')
         
-csv.close()
+uCsv.close()
+sCsv.close()
 
 
 
@@ -134,4 +152,31 @@ for row in oSheet:
 for b in range(len(names)):
         temp = names[b]
         uSheet.cell(row=1, column=b+1).value = temp
+
+
+#EXTRA INCASE NEED LATER
+
+#for each row in the sheet
+for row in uSheet:
+
+    #value in first column
+    string = str(row[0].value) 
+
+    # first letter of value in first column
+    first_letter = string[0]
+
+    # last letter of value in first column
+    last_2letter = string[::-1][0:2]
+
+    #removes "11" and "12" questions
+    if not last_2letter.isnumeric():
+        last_letter = string[-1]
+        #only gets questions surrounding competency
+        if "comp" in string and any([x in last_letter for x in comp]):
+            for i in range(uSheet.max_column):
+                sSheet.add(row)
+
+
+#Comp = ["1", "2", "3", "4", "5", "6"]
+#disc = ["7", "8", "9", "10", "11", "12"]
 '''
