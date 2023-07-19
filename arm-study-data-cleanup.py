1cf2a82811ca3b1f0b562bbd8360d5bb1f59ba8b
Jessica H <jessica.herring123@gmail.com>
2023-07-19 13:14:24 -0700

    titles aSheet

--- arm-study-data-cleanup.py
+++ arm-study-data-cleanup.py
@@ -1,6 +1,7 @@
 ### ORGANIZE EXCEL SHEET FOR QUALTRICS ARM MORPHOLOGY STUDY ###
 
 from openpyxl import Workbook, load_workbook
+import math
 
 # old workbook varibales 
 oWorkbook = load_workbook(filename="Documents/REU/old-arm-study-data.xlsx")
@@ -20,8 +21,7 @@ sSheet = sWorkbook.active
 
 def main():
 
-
-    # adds "blank" row to help with pandas processing
+    # adds "blank"/title row to uSheet help with pandas processing
     uSheet["A1"] = "1Title"
 
 
@@ -112,72 +112,24 @@ def main():
 
 
     ## SIMPLIFIED WORKBOOK
-    comp = ["1", "2", "3", "4", "5", "6"]
-    disc = ["7", "8", "9", "10", "11", "12"]
+    
+    disc1, comp1, saf1 = simplify("1")
+    disc2, comp2, saf2 = simplify("2")
+    disc3, comp3, saf3 = simplify("3")
+    disc4, comp4, saf4 = simplify("4")
+    disc5, comp5, saf5 = simplify("5")
+    disc6, comp6, saf6 = simplify("6")
+    disc7, comp7, saf7 = simplify("7")
+    disc8, comp8, saf8 = simplify("8")
+    disc9, comp9, saf9 = simplify("9")
 
-    disc1 = 0
-    comp1 = 0
-    saf1 = 0
-
-    for i in range (1, (aSheet.max_row + 1)):
-        rows = aSheet.iter_cols(min_row=i, min_col=1, max_col=2, values_only=True)
-
-        #values in each column of the row
-        values = [row[0] for row in rows]
-
-        name = values[0]
-
-        #number of the arm 
-        first_letter = str(name)[0]
-
-        last_2letter = name[::-1][0:2][::-1]
-
-        if "1" in first_letter:
-            value = values[1]
-            if "comp" in name: 
-                #discomfort questions 
-                if last_2letter.isnumeric() or any([x in last_2letter for x in disc]):
-                    disc1 += value
-                #competency questions 
-                else:
-                    comp1 += value
-            #safety questions
-            elif "Average" not in str(value): 
-                saf1 += value
-                
-
-    discomfort1 = disc1/6
-    competency1 = comp1/6
-    safety1 = saf1/3
-
-    print(discomfort1, competency1, safety1)
-                    
-
-        
-
-
-
-    #for each row in the sheet
-    #for row in uSheet:
-
-        #value in first column
-    #   string = str(row[0].value) 
-
-        # first letter of value in first column
-    #   first_letter = string[0]
-
-        # last letter of value in first column
-    #   last_2letter = string[::-1][0:2]
-
-        #removes "11" and "12" questions
-    #   if not last_2letter.isnumeric():
-    #       last_letter = string[-1]
-            #only gets questions surrounding competency
-    #       if "comp" in string and any([x in last_letter for x in comp]):
-    #           for i in range(uSheet.max_column):
-    #               sSheet.add(row)
-
-
+    for i in range(1, 28):
+        if i % 3 == 1:
+            insert("A", i, str(math.ceil(i/3)) + "_Discomfort" ) 
+        elif i % 3 == 2:
+            insert("A", i, str(math.ceil(i/3)) + "_Competency" ) 
+        elif i % 3 == 0:
+            insert("A", i, str(math.ceil(i/3)) + "_Safety" ) 
 
 
 
@@ -214,13 +166,15 @@ def main():
     uCsv.close()
     sCsv.close()
 
-def simplify(x):
+
+
+def simplify(num):
     comp = ["1", "2", "3", "4", "5", "6"]
     disc = ["7", "8", "9", "10", "11", "12"]
 
-    disc = 0
-    comp = 0
-    saf = 0
+    disc1 = 0
+    comp1 = 0
+    saf1 = 0
 
     for i in range (1, (aSheet.max_row + 1)):
         rows = aSheet.iter_cols(min_row=i, min_col=1, max_col=2, values_only=True)
@@ -235,27 +189,36 @@ def simplify(x):
 
         last_2letter = name[::-1][0:2][::-1]
 
-        if x in first_letter:
+        if num in first_letter:
             value = values[1]
             if "comp" in name: 
                 #discomfort questions 
                 if last_2letter.isnumeric() or any([x in last_2letter for x in disc]):
-                    disc += value
+                    disc1 += value
                 #competency questions 
                 else:
-                    comp += value
+                    comp1 += value
             #safety questions
             elif "Average" not in str(value): 
-                saf += value
+                saf1 += value
                 
 
-    discomfort = disc/6
-    competency = comp/6
-    safety = saf/3
+    discomfort = disc1/6
+    competency = comp1/6
+    safety = saf1/3
 
     return(discomfort, competency, safety)
 
+
+
+def insert(col, row, val):
+    loc = col + str(row)
+    sSheet[loc] = val
+
+
 main()
+
+
 
 
 '''
